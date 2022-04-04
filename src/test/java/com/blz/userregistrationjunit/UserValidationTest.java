dd79b17c5b665a60578276d0db315538320fda11
Anju-Chandran01 <anjuchandrancs01@gmail.com>
2022-04-04 04:23:09 -0700

    Added : Validated Last Name

--- src/test/java/com/blz/userregistrationjunit/UserValidationTest.java
+++ src/test/java/com/blz/userregistrationjunit/UserValidationTest.java
@@ -8,14 +8,63 @@ public class UserValidationTest {
     @Test
     public void givenFirstName_WhenProper_ShouldReturnTrue() {
         UserValidator validator = new UserValidator();
-        boolean result = validator.validateFirstName("Asim");
+        boolean result = validator.validateFirstName("Anju");
         Assertions.assertTrue(result);
     }
 
+    //Name Written in LowerCase for Testing
     @Test
-    public void givenFirstName_WhenImroper_ShouldReturnFalse() {
+    public void givenFirstName_WhenFirsLetterSmall_ShouldReturnFalse() {
         UserValidator validator = new UserValidator();
-        boolean result = validator.validateFirstName("asim");
+        boolean result = validator.validateFirstName("anju");
+        Assertions.assertFalse(result);
+    }
+
+    //Name Written Less Than 3 Character
+    @Test
+    public void givenFirstName_WhenLessThanThreeCharacter_ShouldReturnFalse() {
+        UserValidator validator = new UserValidator();
+        boolean result = validator.validateFirstName("An");
+        Assertions.assertFalse(result);
+    }
+
+    //Testcase for special Characters
+    @Test
+    public void givenFirstName_WhenSpCharacters_ShouldReturnFalse() {
+        UserValidator validator = new UserValidator();
+        boolean result = validator.validateFirstName("an@#$");
+        Assertions.assertFalse(result);
+    }
+
+    //Last Name In Proper Way
+    @Test
+    public void givenLastName_WhenProper_ShouldReturnTrue() {
+        UserValidator validator = new UserValidator();
+        boolean result = validator.validateLastName("Chandran");
+        Assertions.assertTrue(result);
+    }
+
+    //Test for Lowercase
+    @Test
+    public void givenLastName_WhenLowerCase_ShouldReturnFalse() {
+        UserValidator validator = new UserValidator();
+        boolean result = validator.validateLastName("chandran");
+        Assertions.assertFalse(result);
+    }
+
+    //Test for less than 3 character
+    @Test
+    public void givenLastName_WhenLessThanThreeCharacter_ShouldReturnFalse() {
+        UserValidator validator = new UserValidator();
+        boolean result = validator.validateLastName("ah");
+        Assertions.assertFalse(result);
+    }
+
+    //Test for Special Character
+    @Test
+    public void givenLastName_WhenSpecialCharacter_ShouldReturnFalse() {
+        UserValidator validator = new UserValidator();
+        boolean result = validator.validateLastName("ah@#$%");
         Assertions.assertFalse(result);
     }
 }
