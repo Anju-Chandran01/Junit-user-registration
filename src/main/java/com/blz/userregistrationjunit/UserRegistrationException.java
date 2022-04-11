906c06d592e743bba2e4442692d4c2c7aa9e5ff1
Anju-Chandran01 <anjuchandrancs01@gmail.com>
2022-04-11 12:19:07 -0700

    Added : Exception to handle user input

---
+++ src/main/java/com/blz/userregistrationjunit/UserRegistrationException.java
@@ -0,0 +1,7 @@
+package com.blz.userregistrationjunit;
+
+public class UserRegistrationException extends Exception {
+    public UserRegistrationException(String message) {
+        super(message);
+    }
+}
